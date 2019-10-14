d5d984a2729885a81bd6b1bfe548e28290aa367e
mferiyal <44905071+mferiyal@users.noreply.github.com>
2019-10-14 08:29:18 +0700

    Fragmentjava

---
+++ app/src/main/java/com/mfir/pc/signinfirebase/Fragment1.java
@@ -0,0 +1,22 @@
+package com.mfir.pc.signinfirebase;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment{
+
+
+    View tampilan;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        tampilan = inflater.inflate(R.layout.fragment1_layout,container, false);
+
+        return tampilan;
+    }
+}
