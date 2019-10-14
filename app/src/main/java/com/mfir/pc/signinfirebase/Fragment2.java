297bb8d40c9d5bfddc64820fc64a9ac2fd92bdc7
mferiyal <44905071+mferiyal@users.noreply.github.com>
2019-10-14 10:47:38 +0700

    firebase

--- app/src/main/java/com/mfir/pc/signinfirebase/Fragment2.java
+++ app/src/main/java/com/mfir/pc/signinfirebase/Fragment2.java
@@ -4,17 +4,47 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Fragment2 extends Fragment {
 
     View tampilan;
+    Button signin, signup;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         tampilan = inflater.inflate(R.layout.fragment2_layout,container, false);
+        signin = tampilan.findViewById(R.id.btnsignin);
+        signup = tampilan.findViewById(R.id.btnsignup);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // click signin keluar fragment signin
+                FragmentManager uhu = getFragmentManager();
+                FragmentTransaction ft = uhu.beginTransaction();
+                ft.replace(R.id.framelayout3, new Fragment3());
+                ft.commit();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // click signup keluar fragment signup
+                FragmentManager uhu = getFragmentManager();
+                FragmentTransaction ft = uhu.beginTransaction();
+                ft.replace(R.id.framelayout3, new Fragment4());
+                ft.commit();
+            }
+        });
+
+
         return tampilan;
     }
 }
---
+++ app/src/main/java/com/mfir/pc/signinfirebase/Fragment4.java
@@ -0,0 +1,71 @@
+package com.mfir.pc.signinfirebase;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Fragment4 extends Fragment {
+
+    private FirebaseAuth mAuth;
+    View tampilan;
+    EditText email,password;
+    Button daftar;
+    String TAG = "tag";
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        tampilan = inflater.inflate(R.layout.fragment4_layout, container, false);
+        email = tampilan.findViewById(R.id.edtemail2);
+        password = tampilan.findViewById(R.id.edtpassword2);
+        daftar = tampilan.findViewById(R.id.btnDaftar);
+        mAuth = FirebaseAuth.getInstance();
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loginemail = email.getText().toString();
+                String loginpassword = password.getText().toString();
+
+                // batas fragment firebase
+
+                mAuth.createUserWithEmailAndPassword(loginemail, loginpassword)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(getActivity(),"Authentication success.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(getActivity(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+        });
+           }
+});
+
+return tampilan;
+
+
+    }
+}
